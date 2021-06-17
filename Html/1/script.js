4209edd584df3e732be60a851a40915719f2eebd
MEGH147896325 <83692043+MEGH147896325@users.noreply.github.com>
2021-06-17 22:10:07 +0530

    Add files via upload

---
+++ Html/1/script.js
@@ -0,0 +1,10 @@
+function func1() {
+    window.alert("This is my sister, Neeva");
+}
+
+function func2() {
+    window.alert("This is me ");
+}
+function func3() {
+    window.alert("THANKYOU!");
+}
